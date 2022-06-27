1fda1ea6a62218eaf8d3ab54817cef70fa5aec49
CharlotteNoraJones <47127622+CharlotteNoraJones@users.noreply.github.com>
2022-06-26 21:24:11 -0500

    Changes to the deep comparision script.

--- JS + CSS/RIverside CSD v2 Changes. /window.addEventListener("load", (event) .js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", (event) => {
-    // Changes "Menu" to "Explore"
-    document.querySelectorAll("#menubutton span span")[0].innerText = "Explore";
-  
-
-});
-
--- Javascript/Practice Scripts/deep_comparision.js
+++ Javascript/Practice Scripts/deep_comparision.js
@@ -1,18 +1,19 @@
 // Performs a deep comparision on any two objects. 
 
-let object1 = {left: 1, right: 1};
+let object1 = {left: 1, right: 2};
 let object2 = {left: 4, right: 3};
 let object3 = {here: {is: "an"}, object: 2};
 let object4 = {left: 1, right: 1, up: 1};
 let object5 = {left: object4, right: 1}
 let object6 = 5;
-console.log(deepEqual(object6, 6));
+console.log("Result: " + deepEqual(object1, object2));
 
 
 /* 
 * Deep comparision between two objects.
 * The objecs are equal if the have the same keys with the same value, 
-* the order of the keys does not matter.  
+* the order of the keys does not matter. 
+* Comparison between keys and values type-sensitive. 
 */
 function deepEqual(a, b) {
 
@@ -31,11 +32,28 @@ function deepEqual(a, b) {
     }
 
     // If they are both objects check if they have the same amount of keys. 
-    
+    a_keys = Object.keys(a);
+    b_keys = Object.keys(b);
+    if (a_keys.length != b_keys.length) {
+        return false;
+    }
 
     // Check if all the keys are titled the same. 
-
+    a_keys.forEach((x, i) => {
+        if (!(b_keys.includes(x))) {
+            return false;
+        }
+    })
     // Compare the value of each key. 
+    // By this point, we know that each object has the same keys. 
+    a_keys.forEach((x, i) => {
+        console.log(a[x]);
+        console.log(b[x]);
+        if (!(deepEqual(a[x], b[x]))) {
+            console.log("Hello")
+            return false;
+        }
+    });
 
     return true;
 }
