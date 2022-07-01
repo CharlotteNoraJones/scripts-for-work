40a7049bf5d2e0b3fec564c80adb0a5d1fa4d548
CharlotteNDJ <94930622+CharlotteNDJ@users.noreply.github.com>
2022-07-01 14:55:11 -0500

    Deep Equal works now.

--- Javascript/Practice Scripts/deep_comparision.js
+++ Javascript/Practice Scripts/deep_comparision.js
@@ -1,62 +1,83 @@
 // Performs a deep comparision on any two objects. 
 
-let object1 = {left: 1, right: 2};
-let object2 = {left: 4, right: 3};
-let object3 = {here: {is: "an"}, object: 2};
-let object4 = {left: 1, right: 1, up: 1};
-let object5 = {left: object4, right: 1}
+let object1 = { left: 1, right: 2 };
+let object2 = { left: 4, right: 3 };
+let object3 = { here: { is: "an" }, second_key: 2 };
+let object3alt = { here: { is: "fe" }, second_key: 2 };
+let object4 = { left: 1, right: 1, up: 1 };
+let object5 = { left: object4, right: 1 }
 let object6 = 5;
-console.log("Result: " + deepEqual(object1, object2));
+console.log("Result: " + deepEqual(object3, object3alt));
 
 
-/* 
-* Deep comparision between two objects.
-* The objecs are equal if the have the same keys with the same value, 
-* the order of the keys does not matter. 
-* Comparison between keys and values type-sensitive. 
-*/
 function deepEqual(a, b) {
+    /* 
+    * Deep comparision between two objects.
+    * The objecs are equal if the have the same keys with the same values, 
+    * the order of the keys does not matter. 
+    * Comparison between keys and values is type-sensitive. 
+    * This is a pure function.
+    */
 
-    a_type = typeof a;
-    b_type = typeof b;
-
-    // Check if objects are both of the same type
-    if (a_type != b_type) {
+    if (!checkSameType(a, b)) {
         return false;
     }
 
-    // If they are not objects, compare value. 
-    both_are_not_objects = !((a_type === 'object') && (b_type === 'object'));
-    if (both_are_not_objects) {
-        return a === b;
+    if (!checkBothObjects(a, b)) {
+        return (a === b);
     }
 
-    // If they are both objects check if they have the same amount of keys. 
+    if (!checkObjectKeys(a, b)) {
+        return false;
+    }
+
+    // Check if all values are the same
+    // By this point, we know that all keys are the same. 
     a_keys = Object.keys(a);
     b_keys = Object.keys(b);
-    if (a_keys.length != b_keys.length) {
-        return false;
-    }
 
-    // Check if all the keys are titled the same. 
-    a_keys.forEach((x, i) => {
-        if (!(b_keys.includes(x))) {
-            return false;
-        }
-    })
-    // Compare the value of each key. 
-    // By this point, we know that each object has the same keys. 
-    a_keys.forEach((x, i) => {
-        console.log(a[x]);
-        console.log(b[x]);
-        if (!(deepEqual(a[x], b[x]))) {
-            console.log("Hello")
-            return false;
+    values_equality = true;
+
+    a_keys.forEach((key) => {
+        if(!deepEqual(a[key], b[key])) {
+            values_equality = false;
         }
     });
 
-    return true;
+    return values_equality;
 }
 
+function checkSameType(a, b) {
+    // Returns true if a and b are of the same type. 
+    a_type = typeof a;
+    b_type = typeof b;
 
-// Note to self: Remember that ordering of keys in list is not reliable. 
+    return a_type == b_type;
+
+}
+
+function checkBothObjects(a, b) {
+    // Return true if both a and b are objects. 
+    a_type = typeof a;
+    b_type = typeof b;
+
+    return ((a_type === 'object') && (b_type === 'object'));
+}
+
+function checkObjectKeys(a, b) {
+    // Takes in two objects. Returns true if they have the same keys. 
+    // The order of keys is not considered. 
+
+    let keys_are_equal = true;
+
+    a_keys = Object.keys(a);
+    b_keys = Object.keys(b);
+
+    a_keys.forEach((a_key) => {
+        if (!(b_keys.includes(a_key))) {
+            keys_are_equal = false;
+        }
+    });
+
+    return keys_are_equal;
+}
